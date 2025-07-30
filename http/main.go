4dd543accfc58ad180eeffaaef8b1f9f542d71ca
Lucas Almeida <lucas-lxx@alu.ufc.br>
2025-07-30 10:38:19 -0300

    add the muxHandler from net/http package to register the routes

--- http/main.go
+++ http/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -9,13 +10,34 @@ type api struct {
 	addr string
 }
 
-func (s *api) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (a *api) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from the server\n"))
 }
 
+func (a *api) getIndexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("GET the index handler\n"))
+}
+
+func (a *api) postIndexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("POST the index handler\n"))
+}
+
+func (a *api) postAboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("POST the about handler\n"))
+}
+
+func UrlBuilder(method string, path string) string {
+	return fmt.Sprintf("%s %s", method, path)
+}
+
 func main() {
-	s := &api{addr: ":8080"}
-	if err := http.ListenAndServe(s.addr, s); err != nil {
+	api := &api{addr: ":8080"}
+	mux := http.NewServeMux()
+
+	mux.HandleFunc(UrlBuilder(http.MethodGet, "/"), api.getIndexHandler)
+	mux.HandleFunc(UrlBuilder(http.MethodPost, "/"), api.postIndexHandler)
+	mux.HandleFunc(UrlBuilder(http.MethodPost, "/about"), api.postAboutHandler)
+	if err := http.ListenAndServe(api.addr, mux); err != nil {
 		log.Fatal(err)
 	}
 }
